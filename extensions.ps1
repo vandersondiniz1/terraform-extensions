ad009bdcbab85a61fa45c39f5c8c2be8f4eb6e34
Vanderson Diniz <45218256+vandersondiniz1@users.noreply.github.com>
2024-11-15 18:54:06 -0300

    Update extensions.ps1

--- extensions.ps1
+++ extensions.ps1
@@ -1,1 +1,9 @@
+# Instala o IIS
 Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
+# Habilita o RDP (Remote Desktop Protocol)
+$rdpKey = "HKLM:\System\CurrentControlSet\Control\Terminal Server"
+$rdpValue = "fDenyTSConnections"
+
+# Altera o valor do registro para permitir RDP
+Set-ItemProperty -Path $rdpKey -Name $rdpValue -Value 0
