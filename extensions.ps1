eef24acd8a1e6edc42c2a51fd8f63741406b9926
Vanderson Diniz <45218256+vandersondiniz1@users.noreply.github.com>
2024-11-15 18:57:41 -0300

    Update extensions.ps1

--- extensions.ps1
+++ extensions.ps1
@@ -4,6 +4,11 @@ Install-WindowsFeature -Name Web-Server -IncludeManagementTools
 # Habilita o RDP (Remote Desktop Protocol)
 $rdpKey = "HKLM:\System\CurrentControlSet\Control\Terminal Server"
 $rdpValue = "fDenyTSConnections"
-
-# Altera o valor do registro para permitir RDP
 Set-ItemProperty -Path $rdpKey -Name $rdpValue -Value 0
+
+# Baixar e instalar o 7-Zip
+$7zipUrl = "https://www.7-zip.org/a/7z1900-x64.exe"
+$downloadPath = "C:\Temp\7z1900-x64.exe"
+New-Item -ItemType Directory -Force -Path "C:\Temp"
+Invoke-WebRequest -Uri $7zipUrl -OutFile $downloadPath
+Start-Process -FilePath $downloadPath -ArgumentList "/S" -Wait
